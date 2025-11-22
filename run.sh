8ff6cc10db44e8b6c6db99f46dd562d9850a5922
atlaspaper16 <atlaspaper16@gmail.com>
2025-11-22 16:26:56 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 python main_community.py --dataset amazon-ratings --res 'None' --comres 1 --epochs 1500 --batch_size 512 --min_q 0.6 --del_q 0.1 --runs 10 --metric acc --display_step 100
 python main_community.py --dataset roman-empire --res 'None' --comres 1 --epochs 500 --batch_size 512 --min_q 1.0 --del_q 0.1 --runs 10 --metric acc
 python main_community.py --dataset cora --res 'none' --comres 1 --epochs 200 --hidden_channels 256  --batch_size 128  --min_q 0.1 --del_q 0.2  --train_prop 0.6 --valid_prop 0.2 --rand_split --runs 10 --display_step 100 --metric acc
-python main_community.py --dataset squirrel-filtered --res 'None' --comres 1 --epochs 300 --batch_size 128 --num_layers 0 --min_q 0.4 --del_q 0.1 --runs 10 --metric acc --display_step 100
+
+python main_community.py --dataset squirrel-filtered --res '.1' --comres 1 --epochs 60 --batch_size 512 --num_layers 3 --dropout .5 --min_q .61 --del_q 0.05 --runs 10 --metric acc --display_step 20 --lr .005
+
 
 python main_community.py --dataset chameleon-filtered --res 'none' --comres 1 --epochs 30 --batch_size 256 --num_layers 1 --dropout 0.5 --min_q 0.7 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric acc --lr .001
 python main_community.py --dataset actor --res 'None' --comres 1 --epochs 200 --batch_size 128 --num_layers 3  --dropout 0.8 --min_q 1 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --rand_split --metric acc
