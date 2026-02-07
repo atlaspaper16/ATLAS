9d834c61311a14a1468afd6d66452bfd14306bd6
atlaspaper16 <atlaspaper16@gmail.com>
2026-02-07 12:42:10 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,31 @@
-python main_community.py --dataset amazon-ratings --res 'None' --comres 1 --epochs 1500 --batch_size 512 --min_q 0.6 --del_q 0.1 --runs 10 --metric acc --display_step 100
-python main_community.py --dataset amazon-ratings --res 'None' --comres 1 --epochs 1500 --batch_size 512 --min_q 0.6 --del_q 0.1 --runs 10 --metric acc --display_step 100 --LPF 1
+python main_community.py --dataset amazon-ratings --res 'None' --epochs 1500 --batch_size 512 --min_q 0.6 --del_q 0.1 --runs 10 --metric acc --display_step 100
+python main_community.py --dataset amazon-ratings --res 'None' --epochs 1500 --batch_size 512 --min_q 0.6 --del_q 0.1 --runs 10 --metric acc --display_step 100 --LPF 1
 
-python main_community.py --dataset roman-empire --res 'None' --comres 1 --epochs 600 --batch_size 11600 --min_q 1.0 --del_q 0.1 --runs 10 --metric acc --display_step 100  --lr .01 --hidden_channels 792 --num_layers 3
-python main_community.py --dataset roman-empire --res 'None' --comres 1 --epochs 600 --batch_size 11600 --min_q 1.0 --del_q 0.1 --runs 10 --metric acc --display_step 100  --lr .01 --hidden_channels 792 --num_layers 3 --LPF 1 --NF
+python main_community.py --dataset roman-empire --res 'None' --epochs 600 --batch_size 11600 --min_q 1.0 --del_q 0.1 --runs 10 --metric acc --display_step 100  --lr .01 --hidden_channels 792 --num_layers 3
+python main_community.py --dataset roman-empire --res 'None' --epochs 600 --batch_size 11600 --min_q 1.0 --del_q 0.1 --runs 10 --metric acc --display_step 100  --lr .01 --hidden_channels 792 --num_layers 3 --LPF 1 --NF
 
-python main_community.py --dataset cora --res 'none' --comres 1 --epochs 200 --hidden_channels 256  --batch_size 128  --min_q 0.1 --del_q 0.2  --train_prop 0.6 --valid_prop 0.2 --rand_split --runs 10 --display_step 100 --metric acc
-python main_community.py --dataset cora --res 'none' --comres 1 --epochs 200 --hidden_channels 256  --batch_size 128  --min_q 0.1 --del_q 0.2  --train_prop 0.6 --valid_prop 0.2 --rand_split --runs 10 --display_step 100 --metric acc --NF --LPF 2
+python main_community.py --dataset cora --res 'none' --epochs 200 --hidden_channels 256  --batch_size 128  --min_q 0.1 --del_q 0.2  --train_prop 0.6 --valid_prop 0.2 --rand_split --runs 10 --display_step 100 --metric acc
+python main_community.py --dataset cora --res 'none' --epochs 200 --hidden_channels 256  --batch_size 128  --min_q 0.1 --del_q 0.2  --train_prop 0.6 --valid_prop 0.2 --rand_split --runs 10 --display_step 100 --metric acc --NF --LPF 2
 
-python main_community.py --dataset squirrel-filtered --res '0' --comres 1 --epochs 20 --hidden_channels 600 --batch_size 600 --num_layers 3 --dropout 0.8 --min_q .61 --del_q 0.05 --runs 10 --metric acc --display_step 3 --lr .005 --weight_decay 0
-python main_community.py --dataset squirrel-filtered --res '0' --comres 1 --epochs 20 --hidden_channels 600 --batch_size 600 --num_layers 3 --dropout 0.8 --min_q .61 --del_q 0.05 --runs 10 --metric acc --display_step 3 --lr .005 --weight_decay 0 --LPF 2
+python main_community.py --dataset squirrel-filtered --res '0' --epochs 20 --hidden_channels 600 --batch_size 600 --num_layers 3 --dropout 0.8 --min_q .61 --del_q 0.05 --runs 10 --metric acc --display_step 3 --lr .005 --weight_decay 0
+python main_community.py --dataset squirrel-filtered --res '0' --epochs 20 --hidden_channels 600 --batch_size 600 --num_layers 3 --dropout 0.8 --min_q .61 --del_q 0.05 --runs 10 --metric acc --display_step 3 --lr .005 --weight_decay 0 --LPF 2
 
 
-python main_community.py --dataset chameleon-filtered --res 'none' --comres 1 --epochs 30 --batch_size 256 --num_layers 1 --dropout 0.5 --min_q 0.7 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric acc --lr .001
-python main_community.py --dataset chameleon-filtered --res 'none' --comres 1 --epochs 30 --batch_size 256 --num_layers 1 --dropout 0.5 --min_q 0.7 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric acc --lr .001 --LPF 2
+python main_community.py --dataset chameleon-filtered --res 'none' --epochs 30 --batch_size 256 --num_layers 1 --dropout 0.5 --min_q 0.7 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric acc --lr .001
+python main_community.py --dataset chameleon-filtered --res 'none' --epochs 30 --batch_size 256 --num_layers 1 --dropout 0.5 --min_q 0.7 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric acc --lr .001 --LPF 2
 
-python main_community.py   --dataset actor   --res 'None' --comres 1   --epochs 400   --batch_size 7600   --num_layers 3   --dropout 0.8   --hidden_channels 256   --lr 0.0001 --weight_decay 0.001   --train_prop 0.6 --valid_prop 0.2   --runs 10 --rand_split   --metric acc --display_step 50 --min_q 1 --del_q 0.1 --weight_decay 0.001
-python main_community.py   --dataset actor   --res 'None' --comres 1   --epochs 400   --batch_size 7600   --num_layers 3   --dropout 0.8   --hidden_channels 256   --lr 0.0001 --weight_decay 0.001   --train_prop 0.6 --valid_prop 0.2   --runs 10 --rand_split   --metric acc --display_step 50 --min_q 1 --del_q 0.1 --weight_decay 0.001 --LPF 1
+python main_community.py   --dataset actor   --res 'None'   --epochs 400   --batch_size 7600   --num_layers 3   --dropout 0.8   --hidden_channels 256   --lr 0.0001 --weight_decay 0.001   --train_prop 0.6 --valid_prop 0.2   --runs 10 --rand_split   --metric acc --display_step 50 --min_q 1 --del_q 0.1 --weight_decay 0.001
+python main_community.py   --dataset actor   --res 'None'   --epochs 400   --batch_size 7600   --num_layers 3   --dropout 0.8   --hidden_channels 256   --lr 0.0001 --weight_decay 0.001   --train_prop 0.6 --valid_prop 0.2   --runs 10 --rand_split   --metric acc --display_step 50 --min_q 1 --del_q 0.1 --weight_decay 0.001 --LPF 1
 
-python main_community.py --dataset tolokers --res '0.5 0.75 1 1.364' --comres 1 --epochs 2000 --batch_size 512 --dropout 0.5 --num_layers 2 --min_q 0.3 --del_q 0.1 --metric rocauc --runs 10 --display_step 100
-python main_community.py --dataset tolokers --res '0.5 0.75 1 1.364' --comres 1 --epochs 2000 --batch_size 512 --dropout 0.5 --num_layers 2 --min_q 0.3 --del_q 0.1 --metric rocauc --runs 10 --display_step 100 --LPF 2
-python main_community.py --dataset tolokers --res '0.5 0.75 1 1.364' --comres 1 --epochs 2000 --batch_size 512 --dropout 0.5 --num_layers 2 --min_q 0.3 --del_q 0.1 --metric rocauc --runs 10 --display_step 100 --LPF 2 --NF
+python main_community.py --dataset tolokers --res '0.5 0.75 1 1.364' --epochs 2000 --batch_size 512 --dropout 0.5 --num_layers 2 --min_q 0.3 --del_q 0.1 --metric rocauc --runs 10 --display_step 100
+python main_community.py --dataset tolokers --res '0.5 0.75 1 1.364' --epochs 2000 --batch_size 512 --dropout 0.5 --num_layers 2 --min_q 0.3 --del_q 0.1 --metric rocauc --runs 10 --display_step 100 --LPF 2
+python main_community.py --dataset tolokers --res '0.5 0.75 1 1.364' --epochs 2000 --batch_size 512 --dropout 0.5 --num_layers 2 --min_q 0.3 --del_q 0.1 --metric rocauc --runs 10 --display_step 100 --LPF 2 --NF
 
-python main_community.py --dataset pubmed --res '0.5 1 1.956' --comres 1 --epochs 300 --batch_size 8000 --dropout 0.7 --num_layers 3 --min_q 0.7 --del_q 0.1 --metric acc --runs 10 --display_step 100  --train_prop 0.6 --valid_prop 0.2 --rand_split --lr .001
+python main_community.py --dataset pubmed --res '0.5 1 1.956' --epochs 300 --batch_size 8000 --dropout 0.7 --num_layers 3 --min_q 0.7 --del_q 0.1 --metric acc --runs 10 --display_step 100  --train_prop 0.6 --valid_prop 0.2 --rand_split --lr .001
 
-python main_community.py --dataset questions --res 'none' --comres 1 --epochs 70 --batch_size 256 --num_layers 0 --dropout 0.8 --min_q 0.5 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric rocauc --lr .0001 --weight_decay 0 --hidden_channels 128
-python main_community.py --dataset questions --res 'none' --comres 1 --epochs 70 --batch_size 256 --num_layers 0 --dropout 0.8 --min_q 0.5 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric rocauc --lr .0001 --weight_decay 0 --hidden_channels 128 --LPF 2
-python main_community.py --dataset questions --res 'none' --comres 1 --epochs 70 --batch_size 256 --num_layers 0 --dropout 0.8 --min_q 0.7 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric rocauc --lr .0001 --weight_decay 0 --hidden_channels 128 --LPF 2 --NF
+python main_community.py --dataset questions --res '.3' --epochs 100 --batch_size 256 --num_layers 0 --dropout 0.8 --min_q 0.5 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric rocauc --lr .0001 --weight_decay 0.0000005 --hidden_channels 128  --display_step 20
+python main_community.py --dataset questions --res '.3' --epochs 100 --batch_size 256 --num_layers 0 --dropout 0.8 --min_q 0.5 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric rocauc --lr .0001 --weight_decay 0.0000005 --hidden_channels 128  --display_step 20  --NF
+python main_community.py --dataset questions --res '.3' --epochs 100 --batch_size 256 --num_layers 0 --dropout 0.8 --min_q 0.5 --del_q 0.1 --train_prop 0.6 --valid_prop 0.2 --runs 10 --metric rocauc --lr .0001 --weight_decay 0.0000005 --hidden_channels 128  --display_step 20 --LPF 1 --NF
 
 
 
